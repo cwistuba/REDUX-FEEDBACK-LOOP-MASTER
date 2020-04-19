d77a1cf4afcc93dd19f34b80af5990418df39df0
Caleb Wistuba <calebwistuba@gmail.com>
2020-04-19 08:28:18 -0500

    set up submit button to go to thank you page, thank you page button takes user to beginning of survey

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const app = express();
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 5000;
 
-const resultsRouter = require("./routes/results/results.router");
+const resultsRouter = require("./routes/results.router");
 
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for angular requests
--- src/components/Pages/ThankYouPage/ThankYouPage.js
+++ src/components/Pages/ThankYouPage/ThankYouPage.js
@@ -1,10 +1,16 @@
 import React, { Component } from "react";
 
 class ThankYouPage extends Component {
+  returnHome = (event) => {
+    this.props.history.push("/");
+  };
   render() {
     return (
       <div>
-        <h1>ThankYouPage</h1>
+        <h1>Thank You!</h1>
+        <div>
+          <button onClick={this.returnHome}>Leave New Feedback</button>
+        </div>
       </div>
     );
   }
